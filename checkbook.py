9106f9019abb03c053aaedcd574e08f88b3c303f
Aaron Moore <aaron.mv.moore@gmail.com>
2023-02-13 15:54:13 -0600

    Added an additional menu item transaction history

--- checkbook.py
+++ checkbook.py
@@ -95,6 +95,16 @@ def credit():
     new_balance = lines[-1]['balance']
     return print(f'Your new balance is ${new_balance} \n')
 
+# Transaction history function
+def transaction_history():
+    with open('checkbook.csv', 'r') as f:
+        contents = csv.DictReader(f, fieldnames=fields)
+        lines = [line for line in contents][1:]
+    print('Below is your transaction history\n')
+    for line in lines:
+        print(line)
+    return 
+
 # exiting function
 def goodbye():
     return print('~~~ Thank you for using the terminal checkbook! ~~~') 
@@ -103,7 +113,7 @@ def goodbye():
 def terminal_checkbook():
     while True:
         # the introductory screen that asks the user what they would like to do
-        print('What would you like to do?\n\n 1) view current balance\n 2) record a debit (withdraw)\n 3) record a credit (deposit)\n 4) exit \n')
+        print('What would you like to do?\n\n 1) view current balance\n 2) record a debit (withdraw)\n 3) record a credit (deposit)\n 4) transaction history\n 5) exit \n')
         choice = input('Your choice? ')
         # adding controls in case someone enters an invalid 
         if choice.isdigit() == False:
@@ -111,7 +121,7 @@ def terminal_checkbook():
             continue
         elif choice.isdigit():
             choice = int(choice)
-        if choice > 4 or choice < 1:
+        if choice > 5 or choice < 1:
             print('Invalid choice: ', choice, '\n')
         elif choice == 1:
             view_balance()
@@ -120,6 +130,8 @@ def terminal_checkbook():
         elif choice == 3:
             credit()
         elif choice == 4:
+            transaction_history()
+        elif choice == 5:
             goodbye()
             break
     return
@@ -127,7 +139,7 @@ def terminal_checkbook():
 # initializing needed variables
 fields = ['type', 'amount', 'balance']
 dict_base = {
-    'type': None,
+    'type': 'new checkbook',
     'amount': 0,
     'balance':0
 }
